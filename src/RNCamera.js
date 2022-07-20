28f53764616ee8642a41049362cc2e4cb0555552
Rebecca <rebecca.hsu@miraclemobile.com.tw>
2022-07-20 14:23:38 +0800

    fix deprecated ViewPropTypes

--- src/RNCamera.js
+++ src/RNCamera.js
@@ -5,7 +5,7 @@ import {
   findNodeHandle,
   Platform,
   NativeModules,
-  ViewPropTypes,
+  // ViewPropTypes,
   requireNativeComponent,
   View,
   ActivityIndicator,
@@ -14,6 +14,7 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 
+import { ViewPropTypes } from 'deprecated-react-native-prop-types'
 import type { FaceFeature } from './FaceDetector';
 
 const Rationale = PropTypes.shape({
